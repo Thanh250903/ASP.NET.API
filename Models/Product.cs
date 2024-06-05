8fca49419e86900d41b631f8b742abb7276e2ded
Vu Tien Thanh <vutienthanh25092003@gmail.com>
2024-06-04 22:18:53 -0700

    first_commit

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+    public class Product
+    {
+        // Product.cs
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
