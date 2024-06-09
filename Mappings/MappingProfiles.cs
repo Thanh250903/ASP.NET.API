5f16314f3034fc2f1b7c67358ede5690a35f54d2
Vu Tien Thanh <vutienthanh25092003@gmail.com>
2024-06-09 10:03:31 -0700

    finish brach2_ add DTO, repository and interface

---
+++ Mappings/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WebAPI.Models;
+using WebAPI.DTOs;
+
+namespace WebAPI.Mappings
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles() 
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -2,6 +2,7 @@
 {
     public class Category
     {
+        // Category.cs
         public int Id { get; set; }
         public string Name { get; set; }
     }
